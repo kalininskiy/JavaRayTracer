572bb0b04ebdad2f4b2c8b670dca3539da895570
Kalininskiy Ivan <ikalininskiy@s-vfu.ru>
2017-01-31 22:03:11 +0900

    ray tracer

---
+++ src/vdm/ivanhoe/Main.java
@@ -0,0 +1,53 @@
+package vdm.ivanhoe;
+
+import java.awt.*;
+import javax.swing.*;
+import vdm.ivanhoe.JavaRayTracer;
+
+/**
+ *  JavaRayTracer
+ * (c) 2017, code by Ivan "VanDamM" Kalininskiy
+ */
+public class Main {
+    public static void main(String[] args) {
+//        EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+        ImageFrame frame = new ImageFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        Graphics graphics = frame.getGraphics();
+        ImageComponent imageComponent = new ImageComponent();
+
+        imageComponent.paintComponent(graphics);
+
+        graphics.dispose();
+//            }
+//        });
+    }
+}
+
+class ImageFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 640;
+    private static final int DEFAULT_HEIGHT = 640;
+
+    public ImageFrame() {
+        setTitle("JavaRayTracer by Ivan \"VanDamM\" Kalininskiy (2017)");
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
+
+
+class ImageComponent extends JComponent {
+
+    public void paintComponent(Graphics g) {
+        int imageWidth = 4;
+        int imageHeight = 4;
+
+        for (int i = 0; i * imageWidth <= 640; i++)
+            for (int j = 0; j * imageHeight <= 640; j++) {
+                    g.setColor(new Color(i, j, 200));
+                    g.fillRect(i * imageWidth, j * imageHeight, imageWidth, imageHeight);
+                }
+    }
+}
